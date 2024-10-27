e591661c0c46208b4c785a962e500721d3443af7
dendy192 <dtiawan192@gmail.com>
2024-10-28 01:08:39 +0700

    first commit

---
+++ src/main/java/com/dendy/backend_backoffice/BackendBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.dendy.backend_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBackofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBackofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dendy/backend_backoffice/BackendBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dendy.backend_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBackofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
